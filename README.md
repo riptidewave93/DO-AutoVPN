b3318eca5587b56c115139745b657457db6edba8
Chris Blake <chris@servernetworktech.com>
2015-04-05 15:58:55 -0500

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # DO-AutoVPN
-DO-AutoVPN is an automatic VPN instance creation tool utilizing Digital Ocean's services.
+
+DO-AutoVPN is an automatic VPN instance creation tool utilizing Digital Ocean's services. 
+
+About
+-----
+DO-AutoVPN uses a python script to create a Droplet on DigitalOceans infrastructure, sets up an OpenVPN server on said Droplet, and then securely returns the client configuration file to your local system.
+
+The server post-install scripts can be found at https://github.com/riptidewave93/DO-AutoVPN-Server. It is also worth noting that a VPN instance that is left unused for 15 minutes will destroy itself.
+
+Usage
+-----
+1. Clone repo
+2. run deploy.py
+3. Connect to the VPN using the downloaded client.ovpn file
+
+Issues
+-----
+* You tell me!
---
+++ bin/deploy.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3.4
+#
+# DO-AutoVPN Deployment Script Example
+# Created By: Chris Blake (riptidewave93)
+# https://github.com/riptidewave93/DO-AutoVPN
+#
+
+import digitalocean, random, string, sys, time, pycurl
+
+# Change this to your API key to remove input prompt
+DOKey = None
+
+# Start main code
+while DOKey is None:
+	DOKey = input("Please enter a Ditial Ocean API key that has read and write access: ")
+	if len(DOKey) < 60:
+		print('Invalid entry, please try again.')
+	else:
+		DOKey = DOKey.replace(' ','').replace('\n','') # Remove any possible spaces or breaks from the token
+
+# Generate random values used later
+PullPort = random.randint(1000,9999) # Port we will use later to Download the client config
+PullPass = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(20)) # Pass used to auth for client config
+Hostname = 'DO-AutoVPN' + str(random.randint(000,999))
+
+# Load Userdata
+with open ("./userdata.template", "r") as myfile:
+	UserScript = myfile.read()
+	
+# Replace placeholders with their actual value
+UserScript = UserScript.replace('{PORT}', str(PullPort)).replace('{PASS}', PullPass).replace('{DO_TOKEN}', DOKey)
+	
+# Try to create VM
+droplet = digitalocean.Droplet(token=DOKey,
+	name=Hostname,
+	region='nyc3',
+	image='ubuntu-14-04-x64',
+	size_slug='512mb',
+	backups=False,
+	user_data=UserScript)
+	
+try:
+	droplet.create()
+except Exception as e:
+	print('Deploy Error: ' + str(e))
+	sys.exit()
+else:
+	print('VM Deployment Requested... waiting for build to finish.')
+	# Wait for the instance to spinup
+	done = 0
+	while done == 0:
+		actions = droplet.get_actions()
+		for action in actions:
+			action.load()
+			# Once it shows complete, droplet is up and running
+			if 'complete' in action.status:
+				done = 1
+				print('Build finished!');
+				droplet.load() # Reload values
+				InstanceIP = droplet.ip_address # parse out the IP
+		time.sleep(2) # Let's not knock all day on the API
+print('Sleeping to give cloudinit some time')
+time.sleep(60)
+print('Attempting to pull the client OpenVPN configuration file')
+PullLoop = True
+while PullLoop:
+	try:
+		with open('client.ovpn', 'wb') as f:
+			c = pycurl.Curl()
+			c.setopt(c.URL, 'https://' + InstanceIP + ':' + str(PullPort) + '/client.ovpn')
+			c.setopt(c.WRITEDATA, f)
+			c.setopt(pycurl.USERPWD, 'admin:' + PullPass)
+			c.setopt(pycurl.CONNECTTIMEOUT, 60)
+			c.setopt(c.SSL_VERIFYPEER, 0) # Allow self signed cert!
+			c.perform()
+			c.close()
+	except Exception as e:
+		print('An error occurred: ' + str(e))
+		time.sleep(10)
+		continue
+	else:
+		PullLoop = False
+print('File Downloaded! Script Complete. Use the downloaded client.ovpn to connect to your VPN. If you dont within 15 minutes, the Droplet will destroy itself.')
+		
+	
